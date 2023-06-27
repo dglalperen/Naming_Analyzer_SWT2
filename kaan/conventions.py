933f10984318d516accc40f71fb57ed62eb00888
kaan9700 <81586446+kaan9700@users.noreply.github.com>
2023-06-27 15:27:13 +0200

    Syntaktische Analyse wurde verbessert

--- kaan/conventions.py
+++ kaan/conventions.py
@@ -3,11 +3,13 @@ import nltk
 import unittest
 from nltk.corpus import words
 
+# Download the 'words' corpus from nltk
 nltk.download('words')
 
-# Create a set of English words
+# Create a set of English words for later use
 english_words = set(words.words())
 
+# Function to split a compound word into its constituents
 def split_compound_word(word):
     word = word.lower()
     split_words = []
@@ -23,6 +25,7 @@ def split_compound_word(word):
             break
     return split_words
 
+# Define the PEP 8 naming conventions for different types of identifiers
 pep8_naming_conventions = {
     "function": r"^[a-z_][a-z0-9_]{2,30}$",
     "class": r"^[A-Z][a-zA-Z0-9]{2,30}$",
@@ -30,8 +33,7 @@ pep8_naming_conventions = {
     "constant": r"^[A-Z_][A-Z0-9_]{2,30}$",
 }
 
-
-
+# Function to check if a given name is conformant with a given type of PEP 8 naming convention
 def is_name_conformant(name, name_type):
     pattern = pep8_naming_conventions.get(name_type)
     if not pattern:
@@ -39,49 +41,51 @@ def is_name_conformant(name, name_type):
     if not re.match(pattern, name):
         return False
     # Special case for function names, variable names, and constant names: check if the name is a compound word
-    if name_type in ["function_name", "variable_name", "constant_name"] and "_" not in name:
-        return len(split_compound_word(name)) <= 1
+    if name_type in ["function", "variable", "constant"]:
+        parts = name.split('_')
+        for part in parts:
+            if len(split_compound_word(part)) > 1:
+                return False
     return True
 
 
+
+# Define a set of unit tests to check the correctness of the is_name_conformant function
 class TestPep8Conventions(unittest.TestCase):
-
-    def test_module_name(self):
-        self.assertTrue(is_name_conformant("my_module", "module_name"))
-        self.assertFalse(is_name_conformant("MyModule", "module_name"))
-        self.assertFalse(is_name_conformant("myModule", "module_name"))
-        self.assertFalse(is_name_conformant("my-module", "module_name"))
-
+    # Test cases for class names
     def test_class_name(self):
-        self.assertTrue(is_name_conformant("MyClass", "class_name"))
-        self.assertFalse(is_name_conformant("my_class", "class_name"))
-        self.assertFalse(is_name_conformant("myclass", "class_name"))
-        self.assertFalse(is_name_conformant("My_Class", "class_name"))
-        self.assertFalse(is_name_conformant("myClass", "class_name"))
+        self.assertTrue(is_name_conformant("MyClass", "class"))
+        self.assertFalse(is_name_conformant("my_class", "class"))
+        self.assertFalse(is_name_conformant("myclass", "class"))
+        self.assertFalse(is_name_conformant("My_Class", "class"))
+        self.assertFalse(is_name_conformant("myClass", "class"))
 
+    # Test cases for function names
     def test_function_name(self):
-        self.assertTrue(is_name_conformant("my_function", "function_name"))
-        self.assertFalse(is_name_conformant("myFunction", "function_name"))
-        self.assertFalse(is_name_conformant("MyFunction", "function_name"))
-        self.assertFalse(is_name_conformant("my-function", "function_name"))
-        self.assertFalse(is_name_conformant("myfunction", "function_name"))
+        self.assertTrue(is_name_conformant("my_function", "function"))
+        self.assertFalse(is_name_conformant("myFunction", "function"))
+        self.assertFalse(is_name_conformant("MyFunction", "function"))
+        self.assertFalse(is_name_conformant("my-function", "function"))
+        self.assertFalse(is_name_conformant("myfunction", "function"))
+        self.assertFalse((is_name_conformant("my_functiontwo", "function")))
 
+    # Test cases for variable names
     def test_variable_name(self):
-        self.assertTrue(is_name_conformant("my_variable", "variable_name"))
-        self.assertFalse(is_name_conformant("myVariable", "variable_name"))
-        self.assertFalse(is_name_conformant("MyVariable", "variable_name"))
-        self.assertFalse(is_name_conformant("my-variable", "variable_name"))
-        self.assertFalse(is_name_conformant("myvariable", "variable_name"))
+        self.assertTrue(is_name_conformant("my_variable", "variable"))
+        self.assertFalse(is_name_conformant("myVariable", "variable"))
+        self.assertFalse(is_name_conformant("MyVariable", "variable"))
+        self.assertFalse(is_name_conformant("my-variable", "variable"))
+        self.assertFalse(is_name_conformant("myvariable", "variable"))
 
+    # Test cases for constant names
     def test_constant_name(self):
-        self.assertTrue(is_name_conformant("MY_CONSTANT", "constant_name"))
-        self.assertFalse(is_name_conformant("my_constant", "constant_name"))
-        self.assertFalse(is_name_conformant("MyConstant", "constant_name"))
-        self.assertFalse(is_name_conformant("MY-CONSTANT", "constant_name"))
-        self.assertFalse(is_name_conformant("MYCONSTANT", "constant_name"))
+        self.assertTrue(is_name_conformant("MY_CONSTANT", "constant"))
+        self.assertFalse(is_name_conformant("my_constant", "constant"))
+        self.assertFalse(is_name_conformant("MyConstant", "constant"))
+        self.assertFalse(is_name_conformant("MY-CONSTANT", "constant"))
+        self.assertFalse(is_name_conformant("MYCONSTANT", "constant"))
 
 
-    # Add more tests for other name types...
-
+# Run the unit tests
 if __name__ == "__main__":
     unittest.main()

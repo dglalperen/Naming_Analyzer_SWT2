aa5ebf459774d24e78684c68adc4be5ca9ac725c
Alperen Dagli <alperen.dagli43@gmail.com>
2023-08-22 18:43:39 +0200

    refactored prompt

--- semantic_evaluation.py
+++ semantic_evaluation.py
@@ -6,27 +6,31 @@ import logging
 OPEN_API_TOKEN = "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"
 OPEN_API_TOKEN_WITH_GPT4 = "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"
 
+
 def ask_chatgpt(text_chunk, api_key):
     openai.api_key = api_key
 
     prompt = """
     Title: Advanced Python Source Code Naming Analysis
+
     Prompt:
-    As a highly trained AI model developed by OpenAI, your task is to conduct an in-depth analysis of the naming conventions used in a given Python source code. The aim is to evaluate the quality, appropriateness, and consistency of the names used for functions (excluding `__init__` and other dunder methods), classes, and variables (excluding loop iterators like `i` and `j`).
+    As a highly trained AI model developed by OpenAI, your task is to conduct an in-depth analysis of the naming conventions used in a given Python source code. The aim is to evaluate the quality, appropriateness, and consistency of the names used for functions (excluding __init__ and other dunder methods), classes, and variables (excluding loop iterators like i and j).
 
     In this analysis, consider the following criteria:
-    - **Descriptiveness**: The names should be descriptive and relevant to their associated code. For example, a function meant to calculate an average could be named 'calculate_average', while 'calc_avg' might be somewhat less clear, and 'function1' would be vague and misleading.
-    - **Length**: Very short names (like 'a' or 'x') may not provide enough information about what they represent, while very long names can be cumbersome and may be a sign of trying to do too much with a single variable or function.
-    - **Common Misuses**: Names consisting of generic terms (like 'data', 'value') or Python reserved words should be avoided unless their usage is universally understood in the context of programming.
-    - **Consistency**: Check for consistency in naming conventions across the codebase. A name might be semantically correct in isolation but if it's not consistent with the rest of the codebase, it can be confusing for others reading the code.
-    - **Domain-Specific Conventions**: Depending on the purpose of the program, there may be specific naming conventions or practices that should be followed.
 
+    Descriptiveness: The names should be descriptive and relevant to their associated code. For example, a function meant to calculate an average could be named 'calculate_average', while 'calc_avg' might be somewhat less clear, and 'function1' would be vague and misleading.
+    Length: Very short names (like 'a' or 'x') may not provide enough information about what they represent, while very long names can be cumbersome and may be a sign of trying to do too much with a single variable or function.
+    Common Misuses: Names consisting of generic terms (like 'data', 'value') or Python reserved words should be avoided unless their usage is universally understood in the context of programming.
+    Consistency: Check for consistency in naming conventions across the codebase. A name might be semantically correct in isolation but if it's not consistent with the rest of the codebase, it can be confusing for others reading the code.
+    Domain-Specific Conventions: Depending on the purpose of the program, there may be specific naming conventions or practices that should be followed.
     Your analysis results should be formatted as a JSON object, following this schema:
+
+    json
+    Copy code
     {
         "score": "<score>"
     }
-    Here, `<score>` represents the calculated score between 0 and 1, expressed as a decimal number in string format. This score should reflect the overall quality of naming in the codebase, taking into account the factors mentioned above. It is the value of the "score" key in the provided JSON schema.
-    """
+Here, <score> represents the calculated score between 0 and 1, expressed as a decimal number in string format with a granularity of 0.1. This score should reflect the overall quality of naming in the codebase, taking into account the factors mentioned above. It is the value of the "score" key in the provided JSON schema."""
     messages = [
         {"role": "system", "content": prompt},
         {"role": "user", "content": text_chunk},
@@ -43,9 +47,9 @@ def ask_chatgpt(text_chunk, api_key):
     # print(f"Chunk: {text_chunk}")
     # print(f"Score JSON: {score_json}")
 
-    if score_json is None or 'score' not in score_json:
-        print(f'Could not calculate score for chunk: {text_chunk}')
-        return {'score': '0'}
+    if score_json is None or "score" not in score_json:
+        print(f"Could not calculate score for chunk: {text_chunk}")
+        return {"score": "0"}
 
     return score_json
 
@@ -84,6 +88,7 @@ def request_code_improvement(code_snippet):
     improved_code = response["choices"][0]["message"]["content"]
     return improved_code
 
+
 def get_score(data):
     try:
         score = data.get("score")
@@ -91,11 +96,10 @@ def get_score(data):
         if score is not None:
             float(score)
         else:
-            score = 'N/A'  # Or some other default value
+            score = "N/A"  # Or some other default value
     except (AttributeError, ValueError):
-        score = 'N/A'  # Or some other default value
+        score = "N/A"  # Or some other default value
     return score
-
 
 
 def extract_json_from_string(s):
